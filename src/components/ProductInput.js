964c385062f6b0754068c6220c17e4dc49fb6c9a
sj <sujung033131@gmail.com>
2023-07-12 16:10:20 +0900

    0711 FileUpload React

--- src/components/ProductInput.js
+++ src/components/ProductInput.js
@@ -1,73 +1,75 @@
 import { useRef, useState } from "react";
 import { postProduct } from "../api/productAPI";
 
+// 껍데기 생성
 const initState = {
-    title:'',
-    content:'',
-    writer:'',
-    images: []
+    pname : 'Ice Coffee',
+    pdesc : 'Coffee',
+    price : 4000
 }
+
 
 
 const ProductInput = () => {
 
+    // 참조값 물기
     const fileRef = useRef()
+    
+    // 오류 안뜨게 끔 설정
+    const [product , setProduct] = useState({...initState})
 
-    const [board, setBoard] = useState({...initState})
-
+    // change 함수 생성
     const handleChange = (e) => {
-        board[e.target.name] = e.target.value
-
-        setBoard({...board})
+        product[e.target.name] = e.target.value
+        setProduct({...product})
     }
 
+    // save 함수 생성
     const handleClickSave = (e) => {
-
+        
         const formData = new FormData();
 
-        formData.append("title", board.title)
-        formData.append("content", board.content)
-        formData.append("writer", board.writer)
+        // 하나씩 담아줘여함
+        formData.append("pname", product.pname)
+        formData.append("pdesc", product.pdesc)
+        formData.append("price", product.price)
 
         console.dir(fileRef.current)
 
         const arr = fileRef.current.files
-
-        for(let file of arr){
-            formData.append("Files", file)
+        
+        for(let file of arr) {
+            formData.append("files", file)
         }
 
         postProduct(formData)
 
     }
 
+    // clear 함수 생성
     const handleClickClear = (e) => {
-
-        fileRef.current.value = ''
-
+        fileRef.current.value=''
     }
 
-    // fileRef로 input에 접근할 수 있음(?)
     return ( 
         <div>
             <h1>Product Input</h1>
             <div>
-                <input type='text' name='title' value={board.title} onChange={handleChange}></input>
+                <input type="text" name="pname" value={product.pname} onChange={handleChange}></input>
             </div>
             <div>
-                <input type='text' name='content' value={board.content} onChange={handleChange}></input>
+                <input type="text" name="pdesc" value={product.pdesc} onChange={handleChange}></input>
             </div>
             <div>
-                <input type='text' name='writer' value={board.writer} onChange={handleChange}></input>
+                <input type="text" name="price" value={product.price} onChange={handleChange}></input>
             </div>
             <div>
-                <input type="file" ref={fileRef} multiple name="images" value={board.images} onChange={handleChange}></input>
+                <input type="file" ref={fileRef} multiple name="images" onChange={handleChange}></input>
             </div>
             <div>
                 <button onClick={handleClickSave}>Save</button>
                 <button onClick={handleClickClear}>CLEARFILES</button>
             </div>
-
         </div>
      );
 }
